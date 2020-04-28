ab3067b3e0a13c2761033ceb2af0f94de7c87124
Frédéric Delaval-Dupuis <frederic.delaval.dupuis@gmail.com>
2020-04-28 14:32:49 +0200

    Article author added

--- Entity/Article.php
+++ Entity/Article.php
@@ -36,6 +36,11 @@ class Article
      */
     private $publishedAt;
 
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $author;
+
     public function getId(): ?int
     {
         return $this->id;
@@ -85,6 +90,18 @@ class Article
     public function setPublishedAt(?\DateTimeInterface $publishedAt): self
     {
         $this->publishedAt = $publishedAt;
+
+        return $this;
+    }
+
+    public function getAuthor(): ?string
+    {
+        return $this->author;
+    }
+
+    public function setAuthor(string $author): self
+    {
+        $this->author = $author;
 
         return $this;
     }
